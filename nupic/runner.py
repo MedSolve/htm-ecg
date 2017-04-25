c776094971e1f40a4d6a3ba25f2edd360c61d055
Rune Rasmussen <rune@medsolve.dk>
2017-04-25 16:29:25 +1200

    Added dummy test vs traning data

--- nupic/runner.py
+++ nupic/runner.py
@@ -6,7 +6,8 @@ from config import CLASSCONFIG, CONFIG_L1, CONFIG_L2
 RANROW = 10
 
 # Generate random dataset
-DATA = generateRandomSet(RANROW, CONFIG_L1['inputDimensions'], CONFIG_L1['uintType'])
+DATA_TEST = generateRandomSet(RANROW, CONFIG_L1['inputDimensions'], CONFIG_L1['uintType'])
+DATA_TRAINING = generateRandomSet(RANROW, CONFIG_L1['inputDimensions'], CONFIG_L1['uintType'])
 
 # Generate the layers and classifier
 LAYERONE = Layer(CONFIG_L1)
@@ -14,13 +15,13 @@ LAYERTWO = Layer(CONFIG_L2)
 CLASSIFIER = TopNode(CLASSCONFIG)
 
 # Perform actual learning on first layer
-for row in DATA:
+for row in DATA_TEST:
 
     # run learning and get cols as output
     LAYERONE.learn(row['raw'])
 
 # Perform actual learning on second layer
-for row in DATA:
+for row in DATA_TEST:
 
     # get prediction from trained layer
     out_one = LAYERONE.predict(row['raw'], True)
@@ -29,7 +30,7 @@ for row in DATA:
     LAYERTWO.learn(out_one)
 
 # Perform actual learning on third layer
-for row in DATA:
+for row in DATA_TEST:
 
     # get prediction from trained layers
     out_one = LAYERONE.predict(row['raw'], True)
@@ -44,7 +45,7 @@ for row in DATA:
     CLASSIFIER.learn(out_two, bucketIdx, actValue, recordNum)
 
 # Perform inherence on TEST data
-for row in DATA:
+for row in DATA_TRAINING:
 
     out_one = LAYERONE.predict(row['raw'], True)
     out_two = LAYERTWO.predict(out_one, False)
