82acc6a8ad720ab0c9723888d126cd0b00c872b2
Rune Rasmussen <rune@medsolve.dk>
2017-04-26 15:19:33 +1200

    Matlab code done before debug testing

--- matlab/generateSource.m
+++ matlab/generateSource.m
@@ -1,5 +1,8 @@
-% storage path
+% configuration
 storePath = 'data/nupi.source.csv';
+samplingFreq = 500;
+width = 185;
+hight = 96;
 
 % write csv start
 fid = fopen(storePath, 'w') ;
@@ -15,14 +18,73 @@ source = uigetdir;
 % get files of source
 list = dir([source '/*.xml']);
 
+% keep track of global sample number
+sampleNumber = 0;
+
 % loop the source
 for i=1:length(list)
     
     % Get the xml struct
     XMLstruct = ParseXMLFcn([list(i).folder '/' list(i).name]);
     
-    data = XMLstruct;
+    % The data source
+    ecgSource = XMLstruct.RhythmECG.II;
     
-    % Write results
-    %csvwrite(storePath,data,i+1)
+    % get the spans
+    spans = getComplex(ecgSource, samplingFreq, width, hight);
+    
+    %  determine person and set bucket index
+    bucketIdx = str2num(XMLstruct.TestInfo.PatientID);
+    
+    % num spans
+    numSpans = size(spans);
+    
+    % Loop the spans
+    for j=1:numSpans(1)
+        
+        % increment sample number
+        sampleNumber = sampleNumber + 1;
+        
+        % se variables and calculate actual value
+        recordNum = sampleNumber;
+        actValue = bucketIdx + 1/recordNum;
+        
+        % get the raw value
+        raw = spans(j,:);
+        
+        % write result
+        csvwrite(storePath,[recordNum, bucketIdx, actValue, raw],i+1);
+    end
+end
+
+% Get complex spans from source
+function res = getComplex(source, samplingFreq, width, hight)
+
+% use pan tomking to get r wave
+[~,idx,~] = pan_tompkin(source, samplingFreq, 0);
+
+% close all plots
+close all
+
+% set pre and post and remove out of bounds CONFIGURABLE
+pre = idx-width; pre = pre(pre>=1);
+post = idx+width; post = post((1+length(post)-length(pre)):end);
+post = post(post<=length(source));
+pre = pre(1:length(post));
+
+% the scale for encoding
+top = max(source);
+bot = min(source);
+
+% create the empty span
+res = zeros(length(pre),width*2*hight);
+
+% loop all heart beats till end not above ()
+for i=1:length(pre)
+    
+    % save the raw value notice the minux one because of index 0 correction
+    out = imageMap(source(pre(i):(post(i)-1)), bot, top, hight);
+    
+    res(i,:) = out;
+end
 end
--- matlab/imageMap.m
+++ matlab/imageMap.m
@@ -1,17 +1,43 @@
-% Read source image and return a bitmap of that
-function bitmap = imageMap(src) 
+% Read source and return a bitmapish of that normalized to top and bot
+% This is not very effective but does it purpose of creating an image
+% the breaking it down into the bitmapish
+function bitmap = imageMap(src, bot, top, xM)
 
-    % get data from src
-    data = imread(src);
+% get length of source
+yM = length(src);
+
+% create empty data matrix
+data = zeros(yM, xM);
+
+% create search space and scale
+scale = (top-bot)/xM;
+space = (bot+1):scale:top; % +1 to secure correct number of steps
+
+% create image by going through all pixels
+for i = 1:yM
     
-    % get size bounderies
-    [yM, xM] = size(data);
-    
-    % create reference to bitmap
-    bitmap = zeros(1, yM*xM);
-    clc
-    %create a vector by loop the dataset
-    for i = 1:yM
-        bitmap((i-1)*xM+1:i*xM) = data(i,:);
-    end
+    % get the index
+    idx = encodeBit(src(i), space);
+    % set the binery value to 1
+    data(i,idx) = 1;
+end
+
+% get size bounderies
+[yM, xM] = size(data);
+
+% create reference to bitmap
+bitmap = zeros(1, yM*xM);
+
+%create a vector by loop the dataset
+for i = 1:yM
+    bitmap((i-1)*xM+1:i*xM) = data(i,:);
+end
+end
+
+% encodes the bit to its corrospoinding y value with a given resolution
+function idx = encodeBit(bit, space)
+
+    tmp = abs(space-bit);
+    [~, idx] = min(tmp); %index of closest value
+
 end
